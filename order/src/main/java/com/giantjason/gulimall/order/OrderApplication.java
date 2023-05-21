0708425b1ab4dab1aa78205d194ba6134acc740b
GiantJason <104314534+GiantJason@users.noreply.github.com>
2023-05-21 20:58:54 +0800

    initialize program structure

---
+++ order/src/main/java/com/giantjason/gulimall/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.giantjason.gulimall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
---
+++ order/src/test/java/com/giantjason/gulimall/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giantjason.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
